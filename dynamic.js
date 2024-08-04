adb1011aedec2fc27834ffdd30effa925623d460
prasanthchebrolu007 <32529524+prasanthchebrolu007@users.noreply.github.com>
2024-08-03 19:45:49 -0600

    Update dynamic.js

--- dynamic.js
+++ dynamic.js
@@ -3,3 +3,14 @@ console.log("My name is ::" +str );
 
 str = 50;
 console.log("Number is ::" +str)
+
+// JavaScript is known as dynamically typed because the type of a variable can change as the program runs. 
+// Example:
+// Imagine you have a box (variable) that can hold anything. One moment, it can hold a toy car (number), and the next moment, it can hold a teddy bear (string).
+
+// In JavaScript, you can do this:
+
+
+// let box = 5;    // The box holds the number 5
+// box = "hello";  // Now the box holds the word "hello"
+// In this example, the box can change what it holds without any problems, showing that JavaScript doesn't care about the type until you use it.
